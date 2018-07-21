43d221d2e67da09109858c12b9fabcec6fd6a699
mercedesguerrero <m.mercedesguerrero@gmail.com>
2018-07-21 20:15:10 -0300

    +

--- Prueba get string/main.c
+++ Prueba get string/main.c
@@ -3,12 +3,13 @@
 
 void pedirString(char mensaje[], char input[], int limite);
 void getString(char mensaje[], char input[]);
-void getValidString(char mensaje[], char error[], char input[], int limite);
+int getValidString(char mensaje[], char error[], char input[], int limite);
+int esSoloLetras(char str[]);
 
 int main()
 {
     char confirma[3];
-    char nombreIngresado[6];
+    char nombreIngresado[50];
     /*
     pedirString("\nConfirma esta accion? (S/N): ", confirma, 3);
     if(stricmp(confirma, "S") != 0 && stricmp(confirma, "N") != 0)
@@ -19,9 +20,13 @@ int main()
     {
         printf("\nAccion confirmada");
     }
+
     */
-    getValidString("Ingrese nombre\n", "Nombre muy largo\n", nombreIngresado, 6);
-    printf("Nombre ingresado: %s\n", nombreIngresado);
+
+    if((getValidString("\nIngrese nombre: ", "Puede ingresar hasta 6 letras \n", nombreIngresado, 6))>0)
+    {
+        printf("\nNombre ingresado: %s\n", nombreIngresado);
+    }
 
     return 0;
 }
@@ -39,11 +44,31 @@ void getString(char mensaje[], char input[])
     gets(input);
 }
 
+/**
+ * \brief Verifica si el valor recibido contiene solo letras
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si contiene solo ' ' y letras y 0 si no lo es
+ *
+ */
+int esSoloLetras(char str[])
+{
+   int i=0;
+   int retorno=1;
+   while(str[i] != '\0')
+   {
+       if((str[i] != ' ') && (str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
+           retorno=0;
+       i++;
+   }
+   return retorno;
+}
+
 /**< VALIDA TAMAÑO DE STRING */
-void getValidString(char mensaje[], char error[], char input[], int limite)
+int getValidString(char mensaje[], char error[], char input[], int limite)
 {
     int j;
-    char auxString[limite+50]; //por si se pasa
+    char auxString[limite+100]; //por si se pasa
+    int retorno=0;
 
     do
     {
@@ -51,14 +76,16 @@ void getValidString(char mensaje[], char error[], char input[], int limite)
         getString(mensaje, input);
         j= strlen(input);
 
-        if(j>= limite)
+        if(j>= limite || esSoloLetras(input)==0)
         {
-            printf("Ha ingresado %d caracteres\n %s", j, error);
+            printf("%s", error);
         }
         else
         {
             strcpy(auxString, input);
+            retorno=1;
         }
-    }while(j>= limite);
+    }while(j>= limite || esSoloLetras(input)==0);
 
+    return retorno;
 }
