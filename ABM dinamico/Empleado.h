b2f82344d43601490c701be900d7eb73f08a797b
mercedesguerrero <m.mercedesguerrero@gmail.com>
2018-09-24 00:06:31 -0300

    abm

---
+++ ABM dinamico/Empleado.h
@@ -0,0 +1,45 @@
+
+/*
+struct empleado
+{
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldo;
+};
+*/
+typedef struct
+{
+    int id;
+    char descripcion[20];
+
+}eSector;
+
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldo;
+    int isEmpty;
+    int idSector;
+
+}eEmpleado;
+
+
+int menu();
+void presionarContinuar();
+
+void inicializarEmpleados(eEmpleado *listaEmpleados, int tam);
+int buscarLibre(eEmpleado *listaEmpleados, int tam);
+
+int buscarEmpleado(eEmpleado *listaEmpleados, int tam, int legajo);
+
+void id_to_sector(int id, char sector[]);
+
+void printEmpleado(eEmpleado unEmpleado);
+void printEmpleadoPorReferencia(eEmpleado *unEmpleado);
+void mostrarEmpleados(eEmpleado *listaEmpleados, int tam);
+
+void eliminarEmpleado(eEmpleado *listaEmpleados, int tam);
+void modificarEmpleado(eEmpleado *listaEmpleados, int tam);
---
+++ ABM dinamico/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Empleado.h"
+#define CANTIDAD 10
+
+/**< TAREA: UNA FUNCION QUE ABRA UN ARCHIVO Y GUARDE Y CUANDO SE ABRE EL PROGRAMA QUE LEA DE UN ARCHIVO Y LO CARGUE EN EL ARRAY
+        INFORMES:
+        - MOSTRAR EL EMPLEADO QUE MAS GANA Y MENOS GANA
+        - MOSTRAR TODOS LOS HOMBRES
+        ORDENAR ARRAY
+ */
+
+int main()
+{
+    char seguir= 's';
+    eEmpleado *lista= (eEmpleado*)malloc(sizeof(eEmpleado)*10);
+
+    if(lista==NULL)
+    {
+        exit(1);
+    }
+
+    inicializarEmpleados(lista, CANTIDAD);
+
+    eSector sectores[]= {{1, "Ventas"}, {2, "Compras"}, {3, "RRHH"}};
+
+    /**<  COMO COMPROBAR SI BUSCAR EMPLEADO FUNCIONA OK
+
+    eEmpleado r= {1234, "Juan", 'm', 1000, 1};
+    int indice;
+
+    *(lista+3)= r;
+
+    indice= buscarEmpleado(lista, CANTIDAD, 1234);
+
+    printf("%d", indice);
+
+    */
+
+    do
+    {
+        switch(menu())
+        {
+            case 1:
+                agregarEmpleado(lista, CANTIDAD);
+
+                presionarContinuar();
+                break;
+            case 2:
+                eliminarEmpleado(lista, CANTIDAD);
+
+                presionarContinuar();
+                break;
+            case 3:
+                modificarEmpleado(lista, CANTIDAD);
+
+                presionarContinuar();
+                break;
+            case 4:
+                mostrarEmpleados(lista, CANTIDAD);
+
+                presionarContinuar();
+                break;
+            case 5:
+                seguir= 'n';
+                break;
+        }
+
+    }while(seguir=='s');
+
+
+    return 0;
+}
+
+
